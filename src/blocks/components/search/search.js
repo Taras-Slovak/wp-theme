55b1cbeef0500ea5bc621c889911e2df39c88630
Taras Slovak <slovak.taras@gmail.com>
2022-07-19 15:04:45 +0300

    FINISHED

--- src/blocks/components/search/search.js
+++ src/blocks/components/search/search.js
@@ -1,11 +1,29 @@
 'use strict';
-const getInput = document.querySelector('.search__input');
-const body = document.querySelector('body');
+function searchHovering() {
+  const getInput = document.querySelector('.search__input');
+  const body = document.querySelector('body');
 
-body.addEventListener('mouseover', (e) => {
-  if (e.target.classList.contains('hovering')) {
-    getInput.classList.add('shadow');
+  body.addEventListener('mouseover', (e) => {
+    if (e.target.classList.contains('hovering')) {
+      getInput.classList.add('shadow');
+    } else {
+      getInput.classList.remove('shadow');
+    }
+  });
+}
+searchHovering();
+
+function moveSearchInput() {
+  const getArticles = document.querySelector('.article'),
+    getAside = document.querySelector('.aside'),
+    getSearch = document.querySelector('.search');
+  let width = window.innerWidth;
+
+  if (width < 770) {
+    getArticles.prepend(getSearch);
   } else {
-    getInput.classList.remove('shadow');
+    getAside.prepend(getSearch);
   }
-});
+}
+
+moveSearchInput();
